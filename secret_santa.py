543e42010070f454056970d38c661d2faede7abb
Benjamin Huber <huberbp@dukes.jmu.edu>
2022-11-05 22:04:37 -0400

    Made more random

--- secret_santa.py
+++ secret_santa.py
@@ -21,10 +21,31 @@ def random_partition(names, lcaptain, rcaptain):
         names.remove(rand_item)
         r_list.append(rand_item)
 
-    print(r_list)
-    print(l_list)
-
     return [l_list, r_list]
+
+def make_assignments(names):
+    giving = names.copy()
+    recieving = names.copy()
+
+    output = list()
+
+    while len(giving) != 0 and len(recieving) != 0:
+        draw = random.choice(giving)
+        draw2 = random.choice(recieving)
+        loop_check = 0
+        while draw == draw2:
+            draw = random.choice(giving)
+            draw2 = random.choice(recieving)
+            loop_check = loop_check + 1
+            if loop_check > 99:
+                return "ERROR"
+        
+        giving.remove(draw)
+        recieving.remove(draw2)
+
+        output.append(str(draw + " ==> " + draw2))
+    
+    return output
 
 # File should be opened before hand
 def write_names_to_file(names, file):
@@ -58,11 +79,18 @@ def main():
         print("One of your captains ", cap1, " and ", cap2, " are not in the list")
         return
     
+    group1 = make_assignments(split_lists[0])
+    while group1 == "ERROR":
+        group1 = make_assignments(split_lists[0])
+    group2 = make_assignments(split_lists[1])
+    while group2 == "ERROR":
+        group2 = make_assignments(split_lists[1])
+
     left_group_file = open(cap2 + "_group.txt", "w")
     right_group_file = open(cap1 + "_group.txt", "w")
 
-    write_names_to_file(split_lists[0], left_group_file)
-    write_names_to_file(split_lists[1], right_group_file)
+    write_names_to_file(group1, left_group_file)
+    write_names_to_file(group2, right_group_file)
 
 if __name__ == "__main__":
     main()
